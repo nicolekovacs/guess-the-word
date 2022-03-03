132414b70196cdc3d1aa381d70616951457992c3
nicole_k <nikolettk98@gmail.com>
2022-03-03 19:13:03 +0100

    Functions to check, validate and capture input

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const span = document.querySelector("span");
 const message = document.querySelector(".message");
 const againButton = document.querySelector(".play-again");
 const word = "magnolia";
+const guessedLetters = [];
 
 const update = function (word) {
     const updateLetters = [];
@@ -21,7 +22,35 @@ update(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
     const guess = letterInput.value;
-    console.log(guess);
-    letterInput.value = ""; 
+    const goodGuess = inputValidator(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+    }
+    letterInput.value = "";
 });
+
+const inputValidator = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "Please enter a letter!";
+    } else if (input.length > 1 ) {
+        message.innerText = "Please enter only one letter!";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please only enter letters!";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You have already guessed this letter. Please try another!";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
